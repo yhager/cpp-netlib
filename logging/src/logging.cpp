bec19e1526e6ed101a3f255829868a76807544c8
Dean Michael Berris <dberris@google.com>
2013-01-27 00:22:22 +1100

    Run clang-format on logging/

--- logging/src/logging.cpp
+++ logging/src/logging.cpp
@@ -11,48 +11,40 @@
 #include <memory>
 #include <network/logging/logging.hpp>
 
-namespace network { namespace logging { 
+namespace network {
+namespace logging {
 
-  const char* log_record::UNKNOWN_FILE_NAME = "unknown";
+const char* log_record::UNKNOWN_FILE_NAME = "unknown";
 
-
-namespace handler 
-{
-  namespace
-  {
-    void std_log_handler( const log_record& log )
-    {
-      std::cerr << "[network " << log.filename() << ":" << log.line() << "] " 
-                << log.message() << std::endl;
-    }
-  }
-
-  log_record_handler get_std_log_handler() { return &std_log_handler; }
-  log_record_handler get_default_log_handler() { return &std_log_handler; }
+namespace handler {
+namespace {
+void std_log_handler(const log_record& log) {
+  std::cerr << "[network " << log.filename() << ":" << log.line() << "] "
+            << log.message() << std::endl;
+}
 }
 
+log_record_handler get_std_log_handler() { return &std_log_handler; }
+log_record_handler get_default_log_handler() { return &std_log_handler; }
+}
 
-namespace 
-{
-  // the log handler have to manage itself the thread safety on call
-  static auto current_log_record_handler = std::make_shared<log_record_handler>( &handler::std_log_handler );
+namespace {
+// the log handler have to manage itself the thread safety on call
+static auto current_log_record_handler = std::make_shared<log_record_handler>(
+                                             &handler::std_log_handler);
 
 }
 
-  
-void set_log_record_handler( log_record_handler handler )
-{
-  current_log_record_handler = std::make_shared<log_record_handler>( handler );
+void set_log_record_handler(log_record_handler handler) {
+  current_log_record_handler = std::make_shared<log_record_handler>(handler);
 }
 
-void log( const log_record& log )
-{
+void log(const log_record& log) {
   auto log_handler = current_log_record_handler;
-  if( log_handler )
-  {
-  (*log_handler)( log );
+  if (log_handler) {
+    (*log_handler)(log);
   }
 }
 
-
-}}
+}
+}
--- logging/test/logging_custom_handler.cpp
+++ logging/test/logging_custom_handler.cpp
@@ -13,23 +13,25 @@
 using namespace network::logging;
 
 TEST(logging_custom_handler, custom_log_handler_output) {
-  
+
   std::stringstream log_output;
-  auto custom_log_handler = [&]( const log_record& log )
-  {
+  auto custom_log_handler = [&](const log_record & log) {
     log_output << "[CPPNETLIB]<" << log.filename() << ":" << log.line() << "> "
                << log.message();
-  };
+  }
+  ;
 
   const auto line_num = 42;
   const auto file_name = "somewhere.cpp";
-  const auto message = "At line " + std::to_string(line_num) + " we check the code.";
+  const auto message = "At line " + std::to_string(line_num) +
+                       " we check the code.";
 
-  set_log_record_handler( custom_log_handler );
-  log( log_record( file_name, line_num ) << "At line " << line_num << " we check the code." );
+  set_log_record_handler(custom_log_handler);
+  log(log_record(file_name, line_num) << "At line " << line_num
+                                      << " we check the code.");
 
   const auto result_output = log_output.str();
 
-  ASSERT_TRUE( !result_output.empty() );
-  ASSERT_TRUE( result_output == "[CPPNETLIB]<somewhere.cpp:42> " + message );
+  ASSERT_TRUE(!result_output.empty());
+  ASSERT_TRUE(result_output == "[CPPNETLIB]<somewhere.cpp:42> " + message);
 }
