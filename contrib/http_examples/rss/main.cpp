321041f6b9b8e9e2c37b61155d35f3a89ec539cf
Dean Michael Berris <dberris@google.com>
2013-01-27 00:18:42 +1100

    Run clang-format on contrib/

--- contrib/http_examples/rss/main.cpp
+++ contrib/http_examples/rss/main.cpp
@@ -4,36 +4,36 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
-
 #include "rss.hpp"
 #include <network/http/client.hpp>
 #include <boost/foreach.hpp>
 #include <iostream>
 
-int main(int argc, char * argv[]) {
-    namespace http = network::http;
-	namespace rss = network::rss;
+int main(int argc, char* argv[]) {
+  namespace http = network::http;
+  namespace rss = network::rss;
 
-    if (argc != 2) {
-        std::cout << "Usage: " << argv[0] << " <url>" << std::endl;
-        return 1;
+  if (argc != 2) {
+    std::cout << "Usage: " << argv[0] << " <url>" << std::endl;
+    return 1;
+  }
+
+  try {
+    http::client client;
+    http::client::request request(argv[1]);
+    request << network::header("Connection", "close");
+    http::client::response response = client.get(request);
+    rss::channel channel(response);
+
+    std::cout << "Channel: " << channel.title() << " (" << channel.description()
+              << ")" << std::endl;
+    BOOST_FOREACH(const rss::item& item, channel) {
+      std::cout << item.title() << " (" << item.author() << ")" << std::endl;
     }
+  }
+  catch (std::exception& e) {
+    std::cerr << e.what() << std::endl;
+  }
 
-    try {
-        http::client client;
-        http::client::request request(argv[1]);
-        request << network::header("Connection", "close");
-        http::client::response response = client.get(request);
-        rss::channel channel(response);
-
-        std::cout << "Channel: " << channel.title() << " (" << channel.description() << ")" << std::endl;
-        BOOST_FOREACH(const rss::item &item, channel) {
-            std::cout << item.title() << " (" << item.author() << ")" << std::endl;
-        }
-    }
-    catch (std::exception &e) {
-        std::cerr << e.what() << std::endl;
-    }
-
-    return 0;
+  return 0;
 }
