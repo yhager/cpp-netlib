faade8a78c50e303e5269a0e285a96896aa2b1cd
Glyn Matthews <glyn.matthews@gmail.com>
2013-06-23 12:41:49 +0200

    Cleaned up thread_pool so that I can read it more clearly.

--- concurrency/src/network/concurrency/thread_pool.hpp
+++ concurrency/src/network/concurrency/thread_pool.hpp
@@ -16,38 +16,40 @@
 #include <boost/asio/io_service.hpp>
 
 namespace network {
-namespace concurrency {
+  namespace concurrency {
 
-typedef std::shared_ptr<boost::asio::io_service> io_service_ptr;
-typedef std::shared_ptr<std::vector<std::thread>> worker_threads_ptr;
-typedef std::shared_ptr<boost::asio::io_service::work> sentinel_ptr;
+    typedef std::shared_ptr<boost::asio::io_service> io_service_ptr;
+    typedef std::shared_ptr<std::vector<std::thread>> worker_threads_ptr;
+    typedef std::shared_ptr<boost::asio::io_service::work> sentinel_ptr;
 
-struct thread_pool_pimpl;
+    struct thread_pool {
+      thread_pool(std::size_t threads = 1,
+		  io_service_ptr io_service = io_service_ptr(),
+		  std::vector<std::thread> worker_threads = std::vector<std::thread>());
+      thread_pool(thread_pool const&) = delete;
+      thread_pool(thread_pool && other);
+      ~thread_pool();
 
-struct thread_pool {
-  thread_pool(
-      std::size_t threads = 1,
-      io_service_ptr io_service = io_service_ptr(),
-      std::vector<std::thread> worker_threads = std::vector<std::thread>());
-#if !defined(BOOST_NO_CXX11_DELETED_FUNCTIONS)
-  thread_pool(thread_pool const&) = delete;
-#endif  // !defined(BOOST_NO_CXX11_DELETEED_FUNCTIONS)
-  thread_pool(thread_pool && other);
-#if !defined(BOOST_NO_CXX11_DELETED_FUNCTIONS)
-  thread_pool& operator=(thread_pool const&) = delete;
-#endif  // !defined(BOOST_NO_CXX11_DELETEED_FUNCTIONS)
-  thread_pool& operator=(thread_pool && other);
-  std::size_t const thread_count() const;
-  void post(std::function<void()> f);
-  ~thread_pool();
-  void swap(thread_pool& other);
- protected:
-  thread_pool_pimpl* pimpl;
-};
+      thread_pool& operator=(thread_pool const&) = delete;
+      thread_pool& operator=(thread_pool && other);
 
-inline void swap(thread_pool& l, thread_pool& r) { l.swap(r); }
+      std::size_t const thread_count() const;
+      void post(std::function<void()> f);
+      void swap(thread_pool& other);
 
-}  // namespace concurrency
+    private:
+
+      struct pimpl;
+      pimpl* pimpl_;
+
+    };
+
+    inline
+    void swap(thread_pool& l, thread_pool& r) {
+      l.swap(r);
+    }
+
+  }  // namespace concurrency
 }  // namespace network
 
 #endif /* NETWORK_CONCURRENCY_THREAD_POOL_HPP_20101020 */
